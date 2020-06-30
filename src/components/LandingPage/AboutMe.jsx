18078336ac677e796a3b8742942cf2b9de9b0ee4
Sambalicious <stemitope370@gmail.com>
2020-06-30 16:11:05 +0100

    now set

--- src/components/LandingPage/AboutMe.jsx
+++ src/components/LandingPage/AboutMe.jsx
@@ -5,7 +5,7 @@ const AboutMe = () => {
     return (
         <>
         <ScrollAnimation animateIn='fadeIn'  animateOut='fadeOut'>
-        <div  className="text-center px-4 mx-4 mt-4 md:flex">
+        <div  className="text-center px-4 mx-4 mt-6 md:flex">
             <div className="md:mr-2">
                 <img src="https://i.ibb.co/vZQy9dt/undraw-code-thinking-1jeh.png" alt="man_operating_a_computer"/>
             </div>
