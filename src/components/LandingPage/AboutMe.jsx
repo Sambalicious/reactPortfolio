ed8d887cf11e4fefcd43075af1547d399c6a11cd
Sambalicious <stemitope370@gmail.com>
2020-07-21 22:45:56 +0100

    fix overflow

--- src/components/LandingPage/AboutMe.jsx
+++ src/components/LandingPage/AboutMe.jsx
@@ -13,29 +13,29 @@ const AboutMe = () => {
         >
         
             
-        <div  className="justify-center px-4 mx-6 mt-20 text-center md:flex">
+        <div  className="justify-center px-4 mx-6 mt-20 overflow-x-hidden text-center md:flex">
             <div className="md:mr-5"
              
              >
                 <img rel="preconnect" loading="lazy" src="https://i.ibb.co/vZQy9dt/undraw-code-thinking-1jeh.png" alt="man_is_code_thinking"/>
             </div>
-            <div className="-mt-10 align-center md:mt-4">
+            <div className="-mt-10 overflow-x-hidden  align-center md:mt-4">
                 <div className="mb-4 text-4xl font-extrabold hover:text-purple-400">About Me</div>
-                <div className="mt-2 text-purple-600"
+                <div className="mt-2 overflow-x-hidden text-purple-600"
                  data-aos="slide-left" 
                  data-aos-offset="200"
                  data-aos-easing="ease-in-cubic" 
                  data-aos-duration="1000"
                  data-aos-delay="20"
                  >I am a passionate, self taught web developer with keen interest in innovative design</div>
-                <div className="text-purple-600"
+                <div className="text-purple-600 overflow-x-hidden"
                  data-aos="slide-right" 
                  data-aos-offset="200"
                  data-aos-easing="ease-in-sine" 
                  data-aos-duration="1000"
                  data-aos-delay="30"
                  > implementation and understanding of technique geared towards optimum user experience.</div>
-                <div className="text-purple-600"
+                <div className="text-purple-600 overflow-x-hidden"
                  data-aos="slide-up" 
                  data-aos-offset="200"
                  data-aos-delay="40"
